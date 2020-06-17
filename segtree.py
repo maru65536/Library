9a2c1d7ba60e70c629115bc75596ec0dbba4a67a
maru65536 <maru65536@gmail.com>
2020-06-17 15:08:15 +0900

    segtree update

--- segtree.py
+++ segtree.py
@@ -1,15 +1,39 @@
-def make_segtree(a): #listからsegtreeを生成して返す
-    import math
-    #bi=最下段の個数　bi2=高さ
+import math
+inf=999
+def segtree(a): #RMQ(Range Minimum Query)
+    #bi=高さ　bi2=最下段の個数
+    global bi 
     bi=math.ceil(math.log2(len(a)))+1
+    global bi2
     bi2=2**(bi-1)
-    seg=[0]*(2*bi2-1) #初期化。0のところに単位元を。
+    seg=[inf]*(2*bi2-1)
     for i in range(len(a)):
-        seg[bi2+i-1]=a[i] #初期値代入
-    #セグ木の構築は下から二段目から、順々に上の段へ
+        seg[bi2+i-1]=a[i]
     for i in range(bi-1):
         for j in range(2**(bi-i-2)):
             b=2**(bi-i-2)-1+j
-            #適宜minその他の関数で置換
-            seg[b]=seg[2*b+1]+seg[2*b+2]
+            seg[b]=min(seg[2*b+1],seg[2*b+2])
     return seg
+
+def seg_update(loc,val): #locの値をvalueに更新
+    loc+=bi2-1
+    seg[loc]=val
+    for i in range(bi):
+        seg[loc]=min(val,seg[loc])
+        loc=(loc-1)//2
+
+def seg_find(l,r,tmp): #[l,r)の最小値を求める、tmpには0入れといて
+    a=bi-math.ceil(math.log2(tmp+2))+1
+    nl=2**(a-1)*(tmp-(2**(bi-a))+1)
+    nr=nl+2**(a-1)
+    if nr<=l or nl>=r:
+        return inf
+    elif l<=nl and nr<=r:
+        return seg[tmp]
+    else:
+        return min(seg_find(l,r,tmp*2+1),seg_find(l,r,tmp*2+2))
+global seg
+seg=segtree([5,4,3,6,1])
+for i in range(7):
+    print(seg_find(0,i+1,0))
+print(seg)
