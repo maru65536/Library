193ffade597838e8e757632954a7f5614a47dd2d
maru65536 <maru65536@gmail.com>
2020-11-24 06:01:33 +0900

    bug fix

--- halfzrk.py
+++ halfzrk.py
@@ -1,10 +1,10 @@
 def halfzrk(l):
     a,b=l[::2],l[1::2]
     a1,b1=[],[]
-    for i in range(len(a)):
+    for i in range(2**len(a)):
         i=bin(i)[2:].zfill(len(a))
         a1.append(sum([a[j]*int(i[j])for j in range(len(a))]))
-    for i in range(len(b)):
+    for i in range(2**len(b)):
         i=bin(i)[2:].zfill(len(b))
         b1.append(sum([b[j]*int(i[j])for j in range(len(b))]))
     return sorted(a1),sorted(b1)
